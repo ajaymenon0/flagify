349d684663538e1f6517f184d400e03c51c995d4
Ajay Menon <ajaymenon0@gmail.com>
2023-07-06 19:56:31 +0530

    feat: router and basic styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/levels",
+    element: <h1>Levels</h1>,
+  },
+  {
+    path: "/quiz",
+    element: <h1>Quiz</h1>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#ffffff",
+        primary: "#2EB1A1",
+        secondary: "#F2BE22",
+        tertiary: "#FFBE18",
+        quaternary: "#F56969",
+        disabled: "#AAAAAA",
+        "tertiary-bg": "#FFBE1866",
+      },
+      dropShadow: {
+        red: "5px 5px 0px 0px #F56969",
+        primary: "5px 5px 0px 0px #01B7A0",
+        disabled: "5px 5px 0px 0px #C0C0C0",
+      },
+    },
   },
   plugins: [],
 };
